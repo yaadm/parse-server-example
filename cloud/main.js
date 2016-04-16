fa994b57dbd133ed03708cce65cef38af98fb6f9
yaadm <masuary@gmail.com>
2016-04-16 15:15:32 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  
+  var query = new Parse.Query(Parse.Installation);
+  query.containedIn('channels', req.params.channels);
+
+  Parse.Push.send({
+    where: query,
+    data: {
+      alert: req.params.data
+    }
+  }, {
+    success: function() {
+      // Push was successful
+      res.success('Hi');
+    },
+    error: function(error) {
+      // Handle error
+      res.error('Hi');
+    }
+  });
+  
 });
